1ae3b912de3a0290b0c6f3722c6a1d1ecfeb65ab
hjarroneDev <hjarrone.dev@gmail.com>
2022-11-30 17:03:57 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'Provider/Index/index.dart';
 import 'Provider/Listas/listas.dart';
 import 'Provider/PageControler/page_control.dart';
+import 'pages/Doc/Excel/excel.dart';
 import 'pages/login/login.dart';
 
 void main() {
@@ -25,7 +26,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: LoginForm()),
+      home: Scaffold(
+        body: ExcelDash(),
+
+        //body: LoginForm()
+      ),
     );
   }
 }
